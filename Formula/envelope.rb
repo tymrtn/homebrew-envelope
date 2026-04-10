29dc5023263b9c82ecee18700ebd9328ecc608e3
tymrtn <ty@tmrtn.com>
2026-04-10 18:06:16 +0200

    feat: add envelope formula (v0.3.0, builds from source)

---
+++ Formula/envelope.rb
@@ -0,0 +1,20 @@
+# Copyright (c) 2026 Tyler Martin
+# Licensed under FSL-1.1-ALv2
+
+class Envelope < Formula
+  desc "BYO mailbox email client with agent-native primitives"
+  homepage "https://github.com/tymrtn/envelope-email"
+  url "https://github.com/tymrtn/envelope-email/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "e91dfa25a3db61986480c49e97caa6d8b3c66899abc2b94aa8cbce1b6a173e90"
+  license "FSL-1.1-ALv2"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/cli")
+  end
+
+  test do
+    assert_match "envelope", shell_output("#{bin}/envelope --version")
+  end
+end
